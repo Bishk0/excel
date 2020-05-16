284203c43e6f24847cde645f4a54ca09d6233018
Bishko <yurik322412@gmail.com>
2020-05-16 15:24:18 +0300

    output of the component template

--- src/components/excel/Excel.js
+++ src/components/excel/Excel.js
@@ -3,4 +3,20 @@ export class Excel {
     this.$el = document.querySelector(selector);
     this.components = options.components || [];
   }
+
+  getRoot() {
+    const $root = document.createElement('div');
+
+    this.components.forEach(Component => {
+      const component = new Component();
+      $root.insertAdjacentHTML('beforeend', component.toHTML());
+    });
+
+    return $root;
+  }
+
+  render() {
+    // this.$el.insertAdjacentHTML('afterbegin', `<>Test</>`);
+    this.$el.append(this.getRoot());
+  }
 }
--- src/components/formula/Formula.js
+++ src/components/formula/Formula.js
@@ -1,3 +1,7 @@
 import { ExcelComponent } from '@core/ExcelComponent';
 
-export class Formula extends ExcelComponent {}
+export class Formula extends ExcelComponent {
+  toHTML() {
+    return `<h1>Formula</h1>`;
+  }
+}
