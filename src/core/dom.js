b38a332ff1247e0ef683b383dbaa4edaef1c168b
Bishko <yurik322412@gmail.com>
2020-05-16 23:04:10 +0300

    class for work with DOM

--- src/core/dom.js
+++ src/core/dom.js
@@ -1,7 +1,41 @@
-class Dom {}
+class Dom {
+  constructor(selector) {
+    this.$el = typeof selector === 'string'
+      ? document.querySelector(selector)
+      : selector;
+  }
 
-export function $() {
-  return new Dom();
+  html(html) {
+    if (typeof html === 'string') {
+      this.$el.innerHTML = html;
+      return this;
+    }
+    return this.$el.outerHTML.trim();
+  }
+
+  clear() {
+    this.html('');
+    return this;
+  }
+
+  on() {
+  }
+
+  append(node) {
+    if (node instanceof Dom) {
+      node = node.$el;
+    }
+    if (Element.prototype.append) {
+      this.$el.append(node);
+    } else {
+      this.$el.appendChild(node);
+    }
+    return this;
+  }
+}
+
+export function $(selector) {
+  return new Dom(selector);
 }
 
 $.create = (tagName, classes = '') => {
@@ -9,5 +43,5 @@ $.create = (tagName, classes = '') => {
   if (classes) {
     el.classList.add(classes);
   }
-  return el;
+  return $(el);
 };
