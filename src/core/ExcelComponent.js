487a69896d29de0877b83b5bfd7d24324f63d222
Bishko <yurik322412@gmail.com>
2020-05-17 00:40:20 +0300

    preparation of the stucture for events

--- src/core/ExcelComponent.js
+++ src/core/ExcelComponent.js
@@ -1,8 +1,16 @@
 import { DomListener } from '@core/DomListener';
 
 export class ExcelComponent extends DomListener {
+  constructor($root, options = {}) {
+    super($root, options.listeners);
+  }
+
   // return template components
   toHTML() {
     return '';
+  }
+
+  init() {
+    this.initDOMListeners();
   }
 }
