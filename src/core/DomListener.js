1b15d8b5fc297310331706bea38155636c32d5ca
Bishko <yurik322412@gmail.com>
2020-05-17 15:41:39 +0300

    add listening events

--- src/core/DomListener.js
+++ src/core/DomListener.js
@@ -1,3 +1,5 @@
+import { capitalize } from '@core/utils';
+
 export class DomListener {
   constructor($root, listeners = []) {
     if (!$root) {
@@ -8,10 +10,23 @@ export class DomListener {
   }
 
   initDOMListeners() {
-    console.log(this.listeners);
+    this.listeners.forEach(listener => {
+      const method = getMethodName(listener);
+      if (!this[method]) {
+        const name = this.name || '';
+        throw new Error(
+            `Method ${method} is not implemented in ${name} Component`,
+        );
+      }
+      this.$root.on(listener, this[method].bind(this));
+    });
   }
 
   removeDOMListeners() {
 
   }
+}
+
+function getMethodName(eventName) {
+  return 'on' + capitalize((eventName));
 }
