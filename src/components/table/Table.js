95e0ad9ff26d4f73c7857b43bce5ecced029eda2
Bishko <yurik322412@gmail.com>
2020-05-17 22:35:21 +0300

    creating a table header

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -1,48 +1,10 @@
 import { ExcelComponent } from '@core/ExcelComponent';
+import { createTable } from '@/components/table/table.template';
 
 export class Table extends ExcelComponent {
   static className = 'excel__table';
 
   toHTML() {
-    return `
-    <div class="row">
-    <div class="row-info"></div>
-    <div class="row-data">
-      <div class="column">A</div>
-      <div class="column">B</div>
-      <div class="column">C</div>
-      <div class="column">D</div>
-      <div class="column">E</div>
-      <div class="column">F</div>
-      <div class="column">G</div>
-      <div class="column">H</div>
-      <div class="column">J</div>
-      <div class="column">K</div>
-      <div class="column">L</div>
-      <div class="column">O</div>
-      <div class="column">S</div>
-      <div class="column">Y</div>
-      <div class="column">U</div>
-    </div>
-  </div>
-
-  <div class="row">
-    <div class="row-info">1</div>
-    <div class="row-data">
-      <div class="cell selected" contenteditable>A1</div>
-      <div class="cell" contenteditable>B1</div>
-      <div class="cell" contenteditable>C1</div>
-    </div>
-  </div>
-
-  <div class="row">
-    <div class="row-info">2</div>
-    <div class="row-data">
-      <div class="cell" contenteditable>A2</div>
-      <div class="cell" contenteditable>B2</div>
-      <div class="cell" contenteditable>C2</div>
-    </div>
-  </div>
-    `;
+    return createTable();
   }
 }
