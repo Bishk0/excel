126b7ceaf19c4d1fc99fc7ff34b3163644220eb8
Bishko <yurik322412@gmail.com>
2020-05-17 23:21:24 +0300

    creating a table cell

--- src/components/table/table.template.js
+++ src/components/table/table.template.js
@@ -3,11 +3,11 @@ const CODES = {
   Z: 90,
 };
 
-// function createCell() {
-//   return `
-//     <div class="cell" contenteditable>A1</div>
-//   `;
-// }
+function toCell() {
+  return `
+     <div class="cell" contenteditable></div>
+   `;
+}
 
 function toColumn(col) {
   return `
@@ -15,10 +15,10 @@ function toColumn(col) {
   `;
 }
 
-function createRow(content) {
+function createRow(index, content) {
   return `
       <div class="row">
-        <div class="row-info"></div>
+        <div class="row-info">${index ? index : ''}</div>
         <div class="row-data">${content}</div>
       </div>
   `;
@@ -28,7 +28,7 @@ function toChar(_, index) {
   return String.fromCharCode(CODES.A + index);
 }
 
-export function createTable(rowsCount = 15) {
+export function createTable(rowsCount) {
   const colsCount = CODES.Z - CODES.A + 1;
   const rows = [];
 
@@ -38,10 +38,11 @@ export function createTable(rowsCount = 15) {
       .map(toColumn)
       .join('');
 
-  rows.push(createRow(cols));
+  rows.push(createRow(null, cols));
 
   for (let i = 0; i < rowsCount; i++) {
-    rows.push(createRow());
+    const cells = new Array(colsCount).fill('').map(toCell).join('');
+    rows.push(createRow(i + 1, cells));
   }
 
   return rows.join('');
