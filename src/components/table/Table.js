1f3197805ee3dc22e09e6de8556783c3120b9d3a
Bishko <yurik322412@gmail.com>
2020-05-21 14:45:47 +0300

    optimization

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -1,6 +1,7 @@
-import { $ } from '@core/dom';
 import { ExcelComponent } from '@core/ExcelComponent';
 import { createTable } from '@/components/table/table.template';
+import { resizeHandler } from '@/components/table/table.resize';
+import { shouldResize } from '@/components/table/table.functions';
 
 export class Table extends ExcelComponent {
   static className = 'excel__table';
@@ -16,33 +17,8 @@ export class Table extends ExcelComponent {
   }
 
   onMousedown(event) {
-    // console.log(event.target.getAttribute('data-resize'));
-    if (event.target.dataset.resize) {
-      const $resizer = $(event.target);
-      // const $parent = $resizer.$el.parentNode;
-      // const $parent = $resizer.$el.closest('.column');
-      const $parent = $resizer.closest('[data-type="resizable"]');
-      const coords = $parent.getCoords();
-      const cells = this.$root
-          .findAll(`[data-col="${$parent.data.col}"]`);
-      const type = $resizer.data.resize;
-
-      document.onmousemove = e => {
-        if (type === 'col') {
-          const delta = e.pageX - coords.right;
-          const value = coords.width + delta;
-          $parent.$el.style.width = value + 'px';
-          cells.forEach(el => el.style.width = value + 'px');
-        } else {
-          const delta = e.pageY - coords.bottom;
-          const value = coords.height + delta;
-          $parent.$el.style.height = value + 'px';
-        }
-      };
-
-      document.onmouseup = () => {
-        document.onmousemove = null;
-      };
+    if (shouldResize(event)) {
+      resizeHandler(this.$root, event);
     }
   }
 }
--- src/core/dom.js
+++ src/core/dom.js
@@ -53,6 +53,12 @@ class Dom {
   findAll(selector) {
     return this.$el.querySelectorAll(selector);
   }
+
+  css(styles = {}) {
+    Object.keys(styles).forEach(key => {
+      this.$el.style[key] = styles[key];
+    });
+  }
 }
 
 export function $(selector) {
