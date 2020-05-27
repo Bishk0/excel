237b1d025eb1c8385567de6175633c2ab4b73ca9
Bishko <yurik322412@gmail.com>
2020-05-27 13:53:12 +0300

    optimization of subscription logic

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -41,10 +41,6 @@ export class Table extends ExcelComponent {
     this.$on('formula:done', () => {
       this.selection.current.focus();
     });
-
-    // this.$subscribe( state => {
-    //   console.log('TableState', state);
-    // });
   }
 
   selectCell($cell) {
--- src/core/ExcelComponent.js
+++ src/core/ExcelComponent.js
@@ -5,9 +5,9 @@ export class ExcelComponent extends DomListener {
     super($root, options.listeners);
     this.name = options.name || '';
     this.emitter = options.emitter;
+    this.subscribe = options.subscribe || [];
     this.store = options.store;
     this.unsubscribers = [];
-    this.storeSub = null;
 
     this.prepare();
   }
@@ -31,8 +31,10 @@ export class ExcelComponent extends DomListener {
     this.store.dispatch(action);
   }
 
-  $subscribe(fn) {
-    this.storeSub = this.store.subscribe(fn);
+  storeChanged() {}
+
+  isWatching(key) {
+    return this.subscribe.includes(key);
   }
 
   init() {
@@ -42,6 +44,5 @@ export class ExcelComponent extends DomListener {
   destroy() {
     this.removeDOMListeners();
     this.unsubscribers.forEach(unsub => unsub());
-    this.storeSub.unsubscribe();
   }
 }
