11dd6ef31f6c39e58f4bd39a792a6869a8fe530f
Bishko <yurik322412@gmail.com>
2020-05-27 22:00:23 +0300

    style changes through store

--- src/components/table/table.template.js
+++ src/components/table/table.template.js
@@ -1,3 +1,6 @@
+import { defaultStyles } from '@/constants';
+import { camelToDashCase } from '@core/utils';
+
 const CODES = {
   A: 65,
   Z: 90,
@@ -19,13 +22,16 @@ function toCell(state, row) {
     const id = `${row}:${col}`;
     const width = getWidth(state.colState, col);
     const data = state.dataState[id];
+    const styles = Object.keys(defaultStyles)
+        .map(key => `${camelToDashCase(key)}: ${defaultStyles[key]}`)
+        .join(';');
     return `
      <div class="cell" 
      contenteditable 
      data-col="${col}"
      data-type="cell" 
      data-id="${id}"
-     style="width: ${width}"
+     style="${styles}; width: ${width}"
      >${data || ''}</div>
    `;
   };
--- src/redux/actions.js
+++ src/redux/actions.js
@@ -1,4 +1,4 @@
-import { CHANGE_TEXT, TABLE_RESIZE } from '@/redux/types';
+import { CHANGE_TEXT, CHANGE_STYLES, TABLE_RESIZE } from '@/redux/types';
 
 export function tableResize(data) {
   return {
@@ -10,6 +10,13 @@ export function tableResize(data) {
 export function changeText(data) {
   return {
     type: CHANGE_TEXT,
+    data,
+  };
+}
+
+export function changeStyles(data) {
+  return {
+    type: CHANGE_STYLES,
     data,
   };
 }
