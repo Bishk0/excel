19c43f483f3c7a427c0d70b7aa090b72f4b63c43
Bishko <yurik322412@gmail.com>
2020-05-29 20:08:52 +0300

    add opening date

--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -2,7 +2,7 @@ import {
   CHANGE_TEXT,
   CHANGE_STYLES,
   TABLE_RESIZE,
-  APPLY_STYLE, CHANGE_TITLE,
+  APPLY_STYLE, CHANGE_TITLE, UPDATE_DATE,
 } from '@/redux/types';
 
 export function rootReducer(state, action) {
@@ -33,6 +33,8 @@ export function rootReducer(state, action) {
       };
     case CHANGE_TITLE:
       return {...state, title: action.data};
+    case UPDATE_DATE:
+      return {...state, openedDate: new Date().toJSON()};
     default: return state;
   }
 }
