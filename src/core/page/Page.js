164e7a1c77459a6522aee20b571bb1f261f6e083
Bishko <yurik322412@gmail.com>
2020-05-30 12:05:10 +0300

    Implement DIP

--- src/core/page/Page.js
+++ src/core/page/Page.js
@@ -1,6 +1,6 @@
 export class Page {
   constructor(params) {
-    this.params = params;
+    this.params = params || Date.now().toString();
   }
   getRoot() {
     throw new Error('Method "getRoot" should be implemented');
